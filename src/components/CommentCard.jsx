1e56899991c8eb7c4e9f05e00256ad4c2698df3b
simonhighfield <highfieldsimon@gmail.com>
2024-05-07 17:29:16 +0100

    CommentCard.jsx returns a div with basic comment info

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,28 +1,21 @@
+import Image from 'react-bootstrap/Image';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
-// import './CommentCard.css'
+import './CommentCard.css'
 
 export default function CommentCard(props) {
     const { comment } = props
 
     return (
         <>
-            <p>stuff</p>
-            {/* <Card className="comment-card">
-                <Card.Img variant="top" src={article.article_img_url} />
-                <Card.Body>
-                    <Card.Title>{article.title}</Card.Title>
-                    <Card.Text>
-                        {article.author} 
-                        <br/>
-                        {article.created_at}
-                    </Card.Text>
-                <Link to={`/articles/${article.article_id}`}>
-                    <Button variant="primary">View Article</Button>
-                </Link>
-                </Card.Body>
-            </Card> */}
+            <div className='comment-card'>
+                <h2>{comment.author}</h2>
+                <h3>{comment.created_at}</h3>
+                <p>{comment.body}</p>
+                <p>Votes: {comment.votes}</p>
+            </div>
+
         </>    
     )
 } 
