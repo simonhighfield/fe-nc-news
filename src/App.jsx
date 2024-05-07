6755fc83938a4c56039f1bd98548818eedbd945e
simonhighfield <highfieldsimon@gmail.com>
2024-05-07 11:15:06 +0100

    Added use effect so that the api call GET /api/articles runs once successfully on load

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+// import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 
--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,12 +1,38 @@
 import ArticleCard from "./ArticleCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const endpoint = "https://nc-news-fswv.onrender.com/api/articles"
 
 export default function ArticleList() {
+    
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        axios.get(endpoint)
+        .then((response) => {
+            setArticles(response.data.articles)
+        })
+    }, [])
+
+
     return (
         <>
             <h2>articles</h2>
-            <ArticleCard></ArticleCard>
-        </>
+            <ul>
+                <Container>
+                    {articles.map((article, index) => {
+                        return (
+                            <p key={index}>{article.title}</p>
 
+                        );
+                    })}
+                </Container>
+            </ul>
+        </>
         
     )
 } 
