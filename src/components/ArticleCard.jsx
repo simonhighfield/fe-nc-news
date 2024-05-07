d07ecd892ae7ce9b5dc04cf8ef6213a3a799a1a6
simonhighfield <highfieldsimon@gmail.com>
2024-05-07 13:03:04 +0100

    Extracted ArticleCard into its own component and stylesheet

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,9 +1,21 @@
-export default function ArticleCard() {
+import Card from 'react-bootstrap/Card';
+import './ArticleCard.css'
+
+export default function ArticleCard(props) {
+    const { article } = props
+
     return (
         <>
-            <h3>article card</h3>
-        </>
-
-        
+            <Card className="article-card">
+                <Card.Img variant="top" src={article.article_img_url} />
+                <Card.Body>
+                    <Card.Title>{article.title}</Card.Title>
+                    <Card.Text>
+                        {article.author} 
+                        <br/>
+                        {article.created_at}</Card.Text>
+                </Card.Body>
+            </Card>
+        </>    
     )
 } 
--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Card from 'react-bootstrap/Card';
 import './ArticleList.css'
 
 const endpoint = "https://nc-news-fswv.onrender.com/api/articles"
@@ -24,31 +23,14 @@ export default function ArticleList() {
     return (
         <>
             <h2>articles</h2>
-            <ul>
-                <Container>
-                    {/* <Row> */}
-                    {articles.map((article, index) => {
-                        return (
-                        // <Col sm="4" key={article.article_id}>
-                            <Card key={article.article_id} className="article-card">
-                                <Card.Img variant="top" src={article.article_img_url} />
-                                <Card.Body>
-                                    <Card.Title>{article.title}</Card.Title>
-                                    <Card.Text>
-                                        {article.author} 
-                                        <br/>
-                                        {article.created_at}</Card.Text>
-                                    {/* <Button variant="primary">Go somewhere</Button> */}
-                                </Card.Body>
-                            </Card>
-       
-                        // </Col>
-                        );
-                    })}
-                    {/* </Row> */}
-                </Container>
+            <ul className="article-list">
+                {articles.map((article, index) => {
+                    return (
+                        <ArticleCard key={article.article_id} article={article}></ArticleCard>
+                        
+                    );
+                })}
             </ul>
         </>
-        
     )
 } 
