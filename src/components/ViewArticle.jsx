017ad86ce10f209b7674d64a03cf29a287dfad62
simonhighfield <highfieldsimon@gmail.com>
2024-05-08 16:39:46 +0100

    Added css class to represent if up or down vote applied

--- src/components/ViewArticle.jsx
+++ src/components/ViewArticle.jsx
@@ -5,6 +5,8 @@ import Loading from './Loading';
 import Error from './Error';
 import ArticleComments from './ArticleComments';
 import handleVote from '../../utils/handleVote';
+import { Button } from 'react-bootstrap';
+import './VoteButton.css'
 
 export default function ViewArticle({ users }) {
     const [isLoading, setIsLoading] = useState(true)
@@ -38,8 +40,8 @@ export default function ViewArticle({ users }) {
                     <h3>{currentArticle.created_at}</h3>
                     <img src={currentArticle.article_img_url}/>
                     <h3>{currentVotes}</h3>
-                    <button onClick={() => handleVote(currentArticle, "up", hasVoted, setHasVoted, currentVotes, setCurrentVotes)}>up-vote</button>
-                    <button onClick={() => handleVote(currentArticle, "down", hasVoted, setHasVoted, currentVotes, setCurrentVotes)}>down-vote</button>
+                    <button onClick={() => handleVote(currentArticle, "up", hasVoted, setHasVoted, currentVotes, setCurrentVotes)} className={hasVoted === "up" ? "selected" : null}>up-vote</button>
+                    <button onClick={() => handleVote(currentArticle, "down", hasVoted, setHasVoted, currentVotes, setCurrentVotes)} className={hasVoted === "down" ? "selected" : null}>down-vote</button>
                     <p>{currentArticle.body}</p>
                 </main>
             }
@@ -48,6 +50,3 @@ export default function ViewArticle({ users }) {
         </>
     )
 } 
-// make it -1, rather than -2
-// highlight whichever is clicked
-// if 404: the vote button is stil there
