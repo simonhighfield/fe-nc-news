cae960e835af34be798fa1de851bfda135c2126d
simonhighfield <highfieldsimon@gmail.com>
2024-05-09 17:02:18 +0100

    Added conditional logic to show 'posting ...' abd disable the button if it is loading

--- src/components/ArticleComments.jsx
+++ src/components/ArticleComments.jsx
@@ -33,15 +33,14 @@ export default function ArticleComments({ article_id, users }) {
             <h2>comments</h2>
             {isLoading ? <Loading /> : 
                 <ul>
-                    <PostCommentCard currentUser={currentUser} article_id={article_id} currentComments={currentComments} setCurrentComments={setCurrentComments}/>
+                    <PostCommentCard currentUser={currentUser} article_id={article_id}  currentComments={currentComments} setCurrentComments={setCurrentComments}/>
                     {currentComments.map((comment) => {
                         let user = getUserData(users, comment.author)   
                         return (
                             <CommentCard key={comment.comment_id} comment={comment} user={user}/>
                         )
                     })}
-                </ul>
-            
+                </ul>   
             }
             {isError ? <Error message={isError}/> : null}
         </section>
--- src/components/PostCommentCard.jsx
+++ src/components/PostCommentCard.jsx
@@ -4,31 +4,40 @@ import AuthorBubble from './AuthorBubble';
 import { useState } from 'react';
 import { commentOnArticle } from '../../utils/api';
 
-export default function PostCommentCard({ currentUser, article_id, newCommentPosted, setNewCommentPosted, currentComments, setCurrentComments }) {
+export default function PostCommentCard({ currentUser, article_id, currentComments, setCurrentComments }) {
     const [newComment, setNewComment] = useState("")
-    const [isLoading, setIsLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(false)
     
     function handleSubmit(event) {
         event.preventDefault();
-        setIsLoading(true)
-        commentOnArticle(article_id, currentUser.username, newComment)
-        .then((postedComment) => {
-            setNewComment("")
-            // Updating the state only refreshes currentComments.map in ArticleComments.jsx; whereas useEffect reloads the whole page
-            setCurrentComments((currentComments) =>
-                [postedComment,...currentComments] 
-            )
-            setIsLoading(false)
-        })
+        if (newComment.length > 0) {
+            setNewComment("posting ...")
+            setIsLoading(true)
+            commentOnArticle(article_id, currentUser.username, newComment)
+            .then((postedComment) => {
+                setNewComment("")
+                // Updating the state only refreshes currentComments.map in ArticleComments.jsx; whereas useEffect reloads the whole page
+                setCurrentComments((currentComments) =>
+                    [postedComment,...currentComments] 
+                )
+                setIsLoading(false)
+            })
+            .catch((err) => {
+                console.log(err.response.data.msg);
+            })
+        }
     }
 
+    function inactiveButton(event) {
+        event.preventDefault();
+    }
 
     return (
         <section className='comment-card' style={{padding: '1em', margin: '1em'}}>
             <AuthorBubble className={'item1'} user={currentUser}/>
             <div className='copy'>
                 <h2 className='item2'>{currentUser.username}</h2>
-                <form className='item5' onSubmit={handleSubmit}>
+                <form className='item5' onSubmit={isLoading === false ? handleSubmit : inactiveButton}>
                     <textarea
                         placeholder='Write a comment ...'
                         value={newComment}
