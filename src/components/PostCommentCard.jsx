708ac6390804a2fdf6f6bf56278a1c64c6b255cb
simonhighfield <highfieldsimon@gmail.com>
2024-05-09 14:30:14 +0100

    relpaced input with a text area and styled it

--- src/components/PostCommentCard.jsx
+++ src/components/PostCommentCard.jsx
@@ -1,4 +1,5 @@
 import './CommentCard.css'
+import './PostCommentCard.css'
 import AuthorBubble from './AuthorBubble';
 import { useState } from 'react';
 import { commentOnArticle } from '../../utils/api';
@@ -7,7 +8,7 @@ import { commentOnArticle } from '../../utils/api';
 
 // steps:
 
-// After post, need to refresh the comment list - this does refresh but it isn't including the new comment
+// render from api? ORRRRRRRR maybe instead of refreshing, get it to be highlighted WHILST posting, and then manually push it to the list using state ... rather than rerender whole thing
 // 3. move currentUser state to the top level
 // 4. get API post working
 // 5. display newly posted comment optimistically? OR comments list should refresh with newly posted
@@ -31,19 +32,13 @@ export default function PostCommentCard({ currentUser, article_id, newCommentPos
         <section className='comment-card' style={{padding: '1em', margin: '1em'}}>
             <AuthorBubble className={'item1'} user={currentUser}/>
             <div className='copy'>
-                <h2 className='item2'>{}</h2>
-                <h3 className='item3'>{}</h3>
-                <p className='item4'>Votes: {}</p>
+                <h2 className='item2'>{currentUser.username}</h2>
                 <form className='item5' onSubmit={handleSubmit}>
-                    <label htmlFor='inputComment'>New Comment</label>
-                    <input 
-                        id='inputComment' 
-                        type="text" 
-                        placeholder='write here ...'
+                    <textarea
+                        placeholder='Write a comment ...'
                         value={newComment}
                         onChange={(event) => {setNewComment(event.target.value)}}
-                    >
-                    </input>
+                    ></textarea>
                     <button>post comment</button>
                 </form>
             </div>
