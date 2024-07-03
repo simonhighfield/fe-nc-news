9369fc6db2dd29ea70855513b01578ad95f01503
simonhighfield <highfieldsimon@gmail.com>
2024-07-03 22:11:44 +0100

    rebranded to Discus

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,14 @@
+
 import TopicsList from "./TopicsList";
 import "./NavBar.css"
+
 
 export default function NavBar(topic, setTopic) {
 
     return (
         <>
             <nav>
-                <h1>NC News Nav Bar</h1>
+                <img className="nav-img" src={"../src/assets/Discus Logo.png"} />
                 <TopicsList topic={topic} setTopic={setTopic}/>
             </nav>
         </>
