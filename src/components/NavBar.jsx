0b48d8bd993f2d38b26a7836be4dd9efadd64add
simonhighfield <highfieldsimon@gmail.com>
2024-07-04 15:37:34 +0100

    Img shows when hosted

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,5 @@
 import TopicsList from "./TopicsList";
 import "./NavBar.css"
-import Select from 'react-select'
 import './OptionsBar.css'
 
 export default function NavBar(topic, setTopic) {
@@ -8,7 +7,7 @@ export default function NavBar(topic, setTopic) {
     return (
         <>
             <nav>
-                <img className="nav-img" src={"../src/assets/discus-logo.png"} />
+                <img className="nav-img" src={'/discus-logo.png'} />
                 <TopicsList topic={topic} setTopic={setTopic}/>
             </nav>
         </>
