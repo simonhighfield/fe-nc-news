7a29e17e43057a76e0833179dafb8c47824db0b0
simonhighfield <highfieldsimon@gmail.com>
2024-07-04 14:40:57 +0100

    renamed logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ export default function NavBar(topic, setTopic) {
     return (
         <>
             <nav>
-                <img className="nav-img" src={"../src/assets/DiscusLogo.png"} />
+                <img className="nav-img" src={"../src/assets/discus-logo.png"} />
                 <TopicsList topic={topic} setTopic={setTopic}/>
             </nav>
         </>
