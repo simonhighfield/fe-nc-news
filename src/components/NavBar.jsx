3d92a56daeadb23c120cabd1a56e866d80b7210e
simonhighfield <highfieldsimon@gmail.com>
2024-07-18 19:41:44 +0100

    logo links to /

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,16 @@
 import TopicsList from "./TopicsList";
 import "./NavBar.css"
 import './OptionsBar.css'
+import { Link, Route } from "react-router-dom";
 
 export default function NavBar() {
 
     return (
         <>
             <nav>
-                <img className="nav-img" src={'/discus-logo.png'} />
+                <Link to={`/`}>
+                    <img className="nav-img" src={'/discus-logo.png'} />
+                </Link>
                 <TopicsList/>
             </nav>
         </>
