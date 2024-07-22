41c9b617415f1edef2192fc7c14db16300c3cdcd
simonhighfield <highfieldsimon@gmail.com>
2024-07-22 18:06:53 +0100

    nav items moved to menu button if small screen

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,38 +2,64 @@ import TopicsList from "./TopicsList";
 import "./NavBar.css"
 import './OptionsBar.css'
 import { Link, Route } from "react-router-dom";
-import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
+import { AiFillHome, AiFillSmile, AiOutlineMenu } from "react-icons/ai";
 import { IconContext } from "react-icons";
+import { useEffect, useState } from "react";
 
-// the nav needs to be max widht of 700
 
 export default function NavBar() {
 
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 600)
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize)
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
+    }, []);
+
+    const handleResize = () => {
+        if (window.innerWidth < 600) {
+            setIsMobile(true)
+        } else {
+            setIsMobile(false)
+        }
+    }
+
     return (
-        <>
-            <nav>
-                <div className="nav-col">
-                    <Link to={`/`}>
-                        <img className="nav-img" src={'/discus-logo.png'} />
-                    </Link>
+        <nav>
+            <div className="nav-col">
+                <Link to={`/`}>
+                    <img className="nav-img" src={'/discus-logo.png'} />
+                </Link>
 
-                    <div className="nav-row">
+                <div className="nav-row">
+                    <IconContext.Provider value={{ size: '2em', color: "#ffde5a", className: "icon" }}>
+                        <div className="icon-div">
+                            <AiFillHome />
+                        </div>
+                    </IconContext.Provider>
+                    
+                    {isMobile ? 
                         <IconContext.Provider value={{ size: '2em', color: "#ffde5a", className: "icon" }}>
-                            <div className="icon-div">
-                                <AiFillHome />
-                            </div>
-                        </IconContext.Provider>
+                        <div className="icon-div">
+                            <AiOutlineMenu />
+                        </div>
+                    </IconContext.Provider>
+                    : 
+                        <>
+                            <TopicsList/>
 
-                        <TopicsList/>
-
-                        <IconContext.Provider value={{ size: '2em', color: "#ffde5a", className: "icon" }}>
-                            <div className="icon-div">
-                                <AiOutlineMenu />
-                            </div>
-                        </IconContext.Provider>
-                    </div>                    
-                </div>
-            </nav>
-        </>
+                            <IconContext.Provider value={{ size: '2em', color: "#ffde5a", className: "icon" }}>
+                                <div className="icon-div">
+                                        logged in as: 
+                                    <AiFillSmile />
+                                </div>
+                            </IconContext.Provider>
+                        </>
+                    }                  
+                </div>                    
+            </div>
+        </nav>
     )
 } 
